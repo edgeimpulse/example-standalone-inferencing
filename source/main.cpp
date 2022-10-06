d10eaab1a6ce011f7060ae2eb607d3d616732b73
Alex E <alex@edgeimpulse.com>
2022-10-06 15:58:52 -0700

    Use ei_printtf, not printf

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,7 @@ static const float features[] = {
 };
 
 int main(int argc, char **argv) {
-    
+
     signal_t signal;            // Wrapper for raw input buffer
     ei_impulse_result_t result; // Used to store inference output
     EI_IMPULSE_ERROR res;       // Return code from inference
@@ -21,9 +21,9 @@ int main(int argc, char **argv) {
 
     // Make sure that the length of the buffer matches expected input length
     if (buf_len != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
-        printf("ERROR: The size of the input buffer is not correct.\r\n");
-        printf("Expected %d items, but got %d\r\n", 
-                EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, 
+        ei_printf("ERROR: The size of the input buffer is not correct.\r\n");
+        ei_printf("Expected %d items, but got %d\r\n",
+                EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE,
                 (int)buf_len);
         return 1;
     }
@@ -36,41 +36,41 @@ int main(int argc, char **argv) {
     res = run_classifier(&signal, &result, false);
 
     // Print return code and how long it took to perform inference
-    printf("run_classifier returned: %d\r\n", res);
-    printf("Timing: DSP %d ms, inference %d ms, anomaly %d ms\r\n", 
-            result.timing.dsp, 
-            result.timing.classification, 
+    ei_printf("run_classifier returned: %d\r\n", res);
+    ei_printf("Timing: DSP %d ms, inference %d ms, anomaly %d ms\r\n",
+            result.timing.dsp,
+            result.timing.classification,
             result.timing.anomaly);
 
     // Print the prediction results (object detection)
 #if EI_CLASSIFIER_OBJECT_DETECTION == 1
-    printf("Object detection bounding boxes:\r\n");
+    ei_printf("Object detection bounding boxes:\r\n");
     for (uint32_t i = 0; i < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; i++) {
         ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
         if (bb.value == 0) {
             continue;
         }
-        printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n", 
-                bb.label, 
-                bb.value, 
-                bb.x, 
-                bb.y, 
-                bb.width, 
+        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
+                bb.label,
+                bb.value,
+                bb.x,
+                bb.y,
+                bb.width,
                 bb.height);
     }
 
     // Print the prediction results (classification)
 #else
-    printf("Predictions:\r\n");
+    ei_printf("Predictions:\r\n");
     for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
-        printf("  %s: ", ei_classifier_inferencing_categories[i]);
-        printf("%.5f\r\n", result.classification[i].value);
+        ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
+        ei_printf("%.5f\r\n", result.classification[i].value);
     }
 #endif
 
     // Print anomaly result (if it exists)
 #if EI_CLASSIFIER_HAS_ANOMALY == 1
-    printf("Anomaly prediction: %.3f\r\n", result.anomaly);
+    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
 #endif
 
     return 0;
