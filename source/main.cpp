f8676f353d4d5c3e0d4c07da693d43140cfc7b9c
Jan Jongboom <janjongboom@gmail.com>
2025-08-14 16:40:09 +0200

    Freeform impulse running

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,25 @@
 #include <stdio.h>
-
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
-
-// Callback function declaration
-static int get_signal_data(size_t offset, size_t length, float *out_ptr);
 
 // Raw features copied from test sample
 static const float features[] = {
-    // Copy raw features here (e.g. from the 'Model testing' page)
+    1, 2, 3, 4, 5, 6, 7, 8
 };
 
 int main(int argc, char **argv) {
 
     signal_t signal;            // Wrapper for raw input buffer
-    ei_impulse_result_t result; // Used to store inference output
+    ei_impulse_result_t result = { 0 }; // Used to store inference output
     EI_IMPULSE_ERROR res;       // Return code from inference
+
+    // alloc freeform outputs (one matrix_t per output tensor)
+    std::vector<matrix_t> freeform_outputs;
+    freeform_outputs.reserve(ei_default_impulse.impulse->freeform_outputs_size);
+    for (size_t ix = 0; ix < ei_default_impulse.impulse->freeform_outputs_size; ++ix) {
+        freeform_outputs.emplace_back(ei_default_impulse.impulse->freeform_outputs[ix], 1);
+    }
+
+    result.freeform_outputs = freeform_outputs.data();
 
     // Calculate the length of the buffer
     size_t buf_len = sizeof(features) / sizeof(features[0]);
@@ -28,9 +33,9 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // Assign callback function to fill buffer used for preprocessing/inference
-    signal.total_length = EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE;
-    signal.get_data = &get_signal_data;
+    run_classifier_init();
+
+    numpy::signal_from_buffer(features, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
 
     // Perform DSP pre-processing and inference
     res = run_classifier(&signal, &result, false);
@@ -42,62 +47,17 @@ int main(int argc, char **argv) {
             result.timing.classification,
             result.timing.anomaly);
 
-    // Print the prediction results (object detection)
-#if EI_CLASSIFIER_OBJECT_DETECTION == 1
-    ei_printf("Object detection bounding boxes:\r\n");
-    for (uint32_t i = 0; i < result.bounding_boxes_count; i++) {
-        ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
-        if (bb.value == 0) {
-            continue;
+    for (size_t ix = 0; ix < freeform_outputs.size(); ix++) {
+        printf("Freeform output index=%d\n", (int)ix);
+        printf("  ");
+        const matrix_t& freeform_output = freeform_outputs[ix];
+        for (size_t jx = 0; jx < freeform_output.rows * freeform_output.cols; jx++) {
+            printf("%f ", freeform_output.buffer[jx]);
         }
-        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
-                bb.label,
-                bb.value,
-                bb.x,
-                bb.y,
-                bb.width,
-                bb.height);
+        printf("\n");
     }
 
-    // Print the prediction results (classification)
-#else
-    ei_printf("Predictions:\r\n");
-    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
-        ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
-        ei_printf("%.5f\r\n", result.classification[i].value);
-    }
-#endif
-
-    // Print anomaly result (if it exists)
-#if EI_CLASSIFIER_HAS_ANOMALY
-    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
-#endif
-
-#if EI_CLASSIFIER_HAS_VISUAL_ANOMALY
-    ei_printf("Visual anomalies:\r\n");
-    for (uint32_t i = 0; i < result.visual_ad_count; i++) {
-        ei_impulse_result_bounding_box_t bb = result.visual_ad_grid_cells[i];
-        if (bb.value == 0) {
-            continue;
-        }
-        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
-                bb.label,
-                bb.value,
-                bb.x,
-                bb.y,
-                bb.width,
-                bb.height);
-    }
-#endif
+    run_classifier_deinit();
 
     return 0;
-}
-
-// Callback: fill a section of the out_ptr buffer when requested
-static int get_signal_data(size_t offset, size_t length, float *out_ptr) {
-    for (size_t i = 0; i < length; i++) {
-        out_ptr[i] = (features + offset)[i];
-    }
-
-    return EIDSP_OK;
 }
